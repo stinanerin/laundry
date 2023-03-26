68acd73d24c3bc4e4b5ceca0bb4a6bd87f73b4a6
stormstina <stina.nerin@gmail.com>
2023-03-26 20:08:22 +0200

    Working day view

--- js/index.js
+++ js/index.js
@@ -1,29 +1,62 @@
 // ----------------------- GLOBAL VARIABLES -----------------------
 const calendar = document.querySelector(".calendar"),
+    dayGrid = document.querySelector(".calender-days-grid"),
+    dayView = document.querySelector(".day-view"),
     dateHeader = document.querySelector(".month-header"),
-    daysContainer = document.querySelector(".calender-days-grid"),
     prevMonth = document.querySelector(".fa-angle-left"),
     nextMonth = document.querySelector(".fa-angle-right"),
     //curtesy of https://gist.github.com/seripap/9eb809268eb8026abd9f
     months = Array.from({length: 12}, (e, i) => {
         return new Date(null, i + 1, null).toLocaleDateString("en", {month: "long"});
     });
-
+    weekdays = Array.from({length: 7}, (e, i) => {
+        return new Date(null, null , i).toLocaleDateString("en", {weekday: "long"});
+    })
+    console.log(weekdays);
 let today = new Date(),
     month = today.getMonth(),
     year = today.getFullYear();
 // ----------------------- GLOBAL FUNCTIONS -----------------------
 
-let createElement = (type, aClass) => {
+let createElement = (type, aClass, str) => {
+    
     let elem = document.createElement(type);
     elem.className = aClass;
+    if(str) {
+        elem.innerText = str
+    }
     return elem;
 }
 
+let renderDayView = () => {
+    console.log("hej");
+
+    let dayBtns = document.querySelectorAll(".day");
+    // console.log(dayBtns);
+    dayBtns.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            console.log("clicked");
+            let button = e.target
+            console.log(e.target.innerText);
+            let monthName = 
+                button.classList.contains("prevMonth") ? months[month - 1]
+                : button.classList.contains("nextMonth") ? months[month + 1]
+                : months[month]
+            dayView.innerHTML = `
+            <h2 class="row g-0">
+                <span class="weekday col">${weekdays[(new Date(year, months.indexOf(monthName), button.innerText)).getDay()]}</span>
+                <span class="date col text-end">${button.innerText} ${monthName}</span>
+            </h2>
+            <div class="row g-0">
+                <button class="time-slot col">08:00</button>
+                <button class="time-slot col">12:00</button>
+                <button class="time-slot col">17:00</button>
+            </div>`
+        })
+    })
+}
+
 let renderMonthCalender = () => {
-    // new Date(year, monthIndex, day, hours, minutes, seconds, milliseconds)
-    // The getDate() method returns the day of the month for the specified date according to local time.
-    // The getDay() method returns the day of the week for the specified date - (0 = sun)
     const firstDay = new Date(year, month, 1),
         // The day of the week for the current date
         day = firstDay.getDay() - 1,
@@ -36,6 +69,9 @@ let renderMonthCalender = () => {
         // The reamining dates, from the next month, which happen in the current months last week
         nextDays = 7 - lastDay.getDay();
 
+    console.log(lastDay);
+    console.log(lastDate);
+    
     // Initates week days counter and container
     let weekDays = 0;
     let row;
@@ -46,39 +82,49 @@ let renderMonthCalender = () => {
         // Creates new row for cal days for each week
         if(weekDays % 7 === 0) {
             row = createElement("div", "row g-0");
-            daysContainer.append(row);
+            dayGrid.append(row);
         }
         weekDays ++;
-        row.innerHTML += `<div class="day col d-flex justify-content-center align-items-center">${prevMontshLastDate - x + 1}</div>`;
+        console.log(weekDays);
+        row.append(createElement("button", "day prevMonth col d-flex justify-content-center align-items-center", prevMontshLastDate - x + 1));
     }
     // Renders the dates from the current month
     for (let x = 1 ; x <= lastDate ; x++) {
         // Creates new row for cal days for each week
         if(weekDays % 7 === 0) {
+            console.log("full week");
+            weekDays = 0
             row = createElement("div", "row g-0");
-            daysContainer.append(row);
+            dayGrid.append(row);
         }
         if(x === today.getDate()) {
-            row.innerHTML += `<div class="today day col d-flex justify-content-center align-items-center">${x}</div>`;
+            row.append(createElement("button", "day col d-flex justify-content-center align-items-center", x));
         } else {
-            row.innerHTML += `<div class="day col d-flex justify-content-center align-items-center">${x}</div>`;
+            row.append(createElement("button", "day col d-flex justify-content-center align-items-center", x));
         }
         weekDays ++;
+        console.log(weekDays);
     }
     // Renders the dates from the next month
     for(let x = 1; x <= nextDays; x++) {
         // Creates new row for cal days for each week
+        //todo! bryt ut
         if(weekDays % 7 === 0) {
             row = createElement("div", "row g-0");
-            daysContainer.append(row);
+            dayGrid.append(row);
         }
         weekDays ++;
-        row.innerHTML += `<div class="day col d-flex justify-content-center align-items-center">${x}</div>`;
+        console.log(weekDays);
+        row.append(createElement("button", "day nextMonth col d-flex justify-content-center align-items-center", x));
     }
 
     // Updates DOM
     dateHeader.innerHTML =  `<h2>${months[month]} ${year}</h2>`;
 
+    renderDayView()
+
 }
 
 renderMonthCalender()
+
+
