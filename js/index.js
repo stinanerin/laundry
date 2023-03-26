3e994fe4f2fec6fb8b432a53254173a20bf1abd3
stormstina <stina.nerin@gmail.com>
2023-03-26 12:05:55 +0200

    Global variables

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-console.log("laundry");
+const calendar = document.querySelector(".calendar"),
+    date = document.querySelector(".month-header"),
+    daysContainer = document.querySelector(".calender-days-grid"),
+    prev = document.querySelector(".fa-angle-left"),
+    next = document.querySelector(".fa-angle-right");
 
+console.log(calendar, date, daysContainer, prev, next);
+
+let today = new Date(),
+    month = today.getMonth(),
+    year = today.getFullYear();
+console.log(today);
+console.log(month);
+console.log(year);
 
 
