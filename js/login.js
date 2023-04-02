95fa215e33de3b9aa3dd3dfe3f9c794dc98ee62c
stormstina <stina.nerin@gmail.com>
2023-04-02 20:18:28 +0200

    Setup login html & js

---
+++ js/login.js
@@ -0,0 +1,13 @@
+
+// ----------------------- LOGIN -----------------------
+
+const loginForm = document.querySelector('#loginForm');
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateLogin(document.querySelector('#loginEmail').value, document.querySelector('#loginPWD').value)
+});
+
+let validateLogin = (email, pwd) => {
+    console.log(email, pwd);
+}
