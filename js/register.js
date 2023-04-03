b1b16802400ac63f5feaefb631b0dda685de718d
stormstina <stina.nerin@gmail.com>
2023-04-03 14:33:42 +0200

    pwd validation with alert and input field error styling

--- js/register.js
+++ js/register.js
@@ -3,7 +3,9 @@ const loginUserLink  = document.querySelector('#loginLink'),
     registerUserLink  = document.querySelector('#registerLink'),
     registerContainer = document.querySelector("#registerWrapper"),
     registerUserForm = document.querySelector("#registerUser"),
-    calender = document.querySelector('#calenderWrapper');
+    calender = document.querySelector('#calenderWrapper'),
+    pwdAlert = document.querySelector("#passwordAlert");
+    // emailAlert = document.querySelector(".reg-alert-container")
 
 let fullName,
     regEmail,
@@ -30,7 +32,6 @@ registerUserForm.addEventListener("submit", (e) => {
 const validateRegisterUser = async(name, email, password, confPwd) => {
     console.log(name, email, password, confPwd);
 
-
     const userArr = await fetchData("6429d84525fc8200e0300328")
     console.log(userArr);
     const checkUniqueUser = user => user.email !== email;
@@ -39,15 +40,30 @@ const validateRegisterUser = async(name, email, password, confPwd) => {
         console.log("create user!");
         // Cretes user in API
         createUser(name, email, password, confPwd);
+        toggleClass([calender, registerContainer], "hidden")
+
     } else if (!userArr.every(checkUniqueUser) && password !== confPwd) {
         console.log("email already registered & pwds do not match");
+        // Hidden
+        pwdAlert.classList.remove("hidden")
+        // Error
+        pwd.classList.add("error");
+        pwdConf.classList.add("error");
+
 
     } else if(!userArr.every(checkUniqueUser)) {
         console.log("email already registered - pwds OK!");
-
+        // Hidden
+        pwdAlert.classList.add("hidden")
+        // Error
+        pwd.classList.remove("error");
+        pwdConf.classList.remove("error");
     } else {
         console.log("pwds do not match - email OK!");
+        // Hidden
+        pwdAlert.classList.remove("hidden")
+        // Error
+        pwd.classList.add("error");
+        pwdConf.classList.add("error");
     }
-
 }
-
